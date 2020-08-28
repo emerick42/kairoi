01d068b2551ffea0345a99c820b1b3970bc22e16
emerick42 <emerick42@pm.me>
2020-08-28 09:28:57 +0200

    Create a persistent storage for the database

--- src/database/query/instruction/mod.rs
+++ src/database/query/instruction/mod.rs
@@ -1,10 +1,12 @@
 mod job;
 mod rule;
 
-use crate::database::execution_context::ExecutionContext;
+use crate::database::storage::Storage;
 use crate::query::instruction::Instruction;
 use job::Set as JobSet;
 use rule::Set as RuleSet;
+use chrono::DateTime;
+use chrono::offset::Utc;
 
 /// Match instructions with statically associated handlers, execute them and return operation
 /// results.
@@ -12,10 +14,10 @@ pub struct Handler {}
 
 impl Handler {
     /// Handle the given instruction and return the operation result.
-    pub fn handle(instruction: &Instruction, context: &mut ExecutionContext) -> Result<(), ()> {
+    pub fn handle(instruction: &Instruction, current_datetime: &DateTime<Utc>, storage: &mut Storage) -> Result<(), ()> {
         match instruction {
-            Instruction::Set { identifier, execution } => JobSet::handle(identifier, execution, context),
-            Instruction::RuleSet { identifier, pattern, runner } => RuleSet::handle(identifier, pattern, runner, context),
+            Instruction::Set { identifier, execution } => JobSet::handle(identifier, execution, current_datetime, storage),
+            Instruction::RuleSet { identifier, pattern, runner } => RuleSet::handle(identifier, pattern, runner, current_datetime, storage),
         }
     }
 }
